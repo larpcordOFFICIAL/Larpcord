bb7b4be42442c8291298eb76807292c97ac73b1f
larpcordOFFICIAL <nadalovesherself@gmail.com>
2026-07-12 01:56:50 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBGM4m7BDtfG6urzcsy9jOCCl8EYGjDEDs",
+  authDomain: "larpcord-8da3e.firebaseapp.com",
+  projectId: "larpcord-8da3e",
+  storageBucket: "larpcord-8da3e.firebasestorage.app",
+  messagingSenderId: "826563140723",
+  appId: "1:826563140723:web:2428450f8f8d3c6f16de06"
+};
