73552ef9ce9398e2e9f20860157d8da2c837c08d
larpcordOFFICIAL <nadalovesherself@gmail.com>
2026-07-13 00:54:07 +0200

    Create giphy.js

---
+++ giphy.js
@@ -0,0 +1,12 @@
+const GIPHY_API_KEY = "08cSmexhrY5nVHE0J3R5bk5ICuUuUnTk";
+
+export async function searchGifs(query) {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${08cSmexhrY5nVHE0J3R5bk5ICuUuUnTk}&q=${encodeURIComponent(query)}&limit=15&rating=pg-13`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.data.map((gif) => ({
+    id: gif.id,
+    preview: gif.images.fixed_height_small.url,
+    full: gif.images.fixed_height.url
+  }));
+}
